a45020b6063431a2e111a573243f94a016aec5f4
theodore-sage <theosage.moro@gmail.com>
2024-08-15 13:48:27 -0400

    initial upload

---
+++ main.js
@@ -0,0 +1,9 @@
+anime({
+    targets: "#name-svg path",
+    strokeDashoffset: [anime.setDashoffset, 700],
+    easing: 'easeInOutQuad',
+    duration: 3750,
+    direction: 'alternate',
+    loop: false,
+    fill: '#000000'
+})
